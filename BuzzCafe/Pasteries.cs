2632162bad0a9fa86f95c97ee3ab9e012083368d
Zeroodfx <kurtfaustino18@gmail.com>
2026-05-10 13:29:00 +0800

    lahat

---
+++ BuzzCafe/Pasteries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BuzzCafe
+{
+    public partial class Pasteries : Form
+    {
+        public Pasteries()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblBeverage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
